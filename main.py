38d5798bc0e1d74e77a37231d133fd107ea3bceb
humag18 <hugo.magerat@gmail.com>
2023-11-25 17:54:14 +0100

    main fixe

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ from firebase_admin import credentials, db
 
 cred = credentials.Certificate("key.json")
 firebase_admin.initialize_app(cred,
-                              {"databaseURL": "https://otolate-bcc65-default-rtdb.europe-west1.firebasedatabase.app/"})
+    {"databaseURL": "https://otolate-bcc65-default-rtdb.europe-west1.firebasedatabase.app/"})
 
 ref_users = db.reference("/users")
 ref_chall = db.reference("/challenges")
+
+# Données de l'utilisateur
+user_data = {
+    "id": 4,
+    "username": "mael",
+    "challenges": ["challenge1", "challenge2"],
+    "score": 0,
+    "message": "t'es super naze"
+}
 
 chall_data = {
     "id": 1002,
@@ -20,6 +29,8 @@ chall_data = {
     "time end": "13:50"
 }
 
+# Ajout de l'utilisateur à la base de données
+ref_users.child(str(user_data["id"])).set(user_data)
 ref_chall.child(str(chall_data["id"])).set(chall_data)
 
 def getUsers():
@@ -35,6 +46,8 @@ def getUsers():
                 name = user_info.get('username', '')
                 score = user_info.get('score', 0)
                 users.append((name, score))
+            else:
+                print("Erreur : user_info est None pour un utilisateur")
     else:
         print("Erreur : users_data n'est pas une liste")
     return users
@@ -52,24 +65,3 @@ def getLastChallenge():
         last_challenge = [challenges_data[last_challenge_id]["content"], challenges_data[last_challenge_id]["output"]]
         return last_challenge
     return "Pas de challenge pour le moment !"
-
-def getUserById(id):
-    user_data = ref_users.child(str(id)).get()
-    return user_data
-
-def getUserByName(name):
-    user_data = ref_users.child(str(name)).get()
-    return user_data
-
-def createUser(name):
-    users = getUsers()
-    id = len(users) + 1
-    user_data = {
-        "id": id,
-        "username": name,
-        "challenges": [],
-        "score": 0,
-        "message": ""
-    }
-    ref_users.child(str(id)).set(user_data)
-    return getUserById(id)
