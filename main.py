9612085ce8b8ac2127cf388e44a5983fc87bff46
n-kmron <cameronnoupoue@gmail.com>
2023-11-25 17:35:49 +0100

    fix home page

--- main.py
+++ main.py
@@ -8,15 +8,6 @@ firebase_admin.initialize_app(cred,
 ref_users = db.reference("/users")
 ref_chall = db.reference("/challenges")
 
-# Données de l'utilisateur
-user_data = {
-    "id": 4,
-    "username": "mael",
-    "challenges": ["challenge1", "challenge2"],
-    "score": 0,
-    "message": "t'es super naze"
-}
-
 chall_data = {
     "id": 1002,
     "content": "arrêt maladie !",
@@ -29,8 +20,6 @@ chall_data = {
     "time end": "13:50"
 }
 
-# Ajout de l'utilisateur à la base de données
-ref_users.child(str(user_data["id"])).set(user_data)
 ref_chall.child(str(chall_data["id"])).set(chall_data)
 
 def getUsers():
@@ -46,8 +35,6 @@ def getUsers():
                 name = user_info.get('username', '')
                 score = user_info.get('score', 0)
                 users.append((name, score))
-            else:
-                print("Erreur : user_info est None pour un utilisateur")
     else:
         print("Erreur : users_data n'est pas une liste")
     return users
@@ -65,3 +52,24 @@ def getLastChallenge():
         last_challenge = [challenges_data[last_challenge_id]["content"], challenges_data[last_challenge_id]["output"]]
         return last_challenge
     return "Pas de challenge pour le moment !"
+
+def getUserById(id):
+    user_data = ref_users.child(str(id)).get()
+    return user_data
+
+def getUserByName(name):
+    user_data = ref_users.child(str(name)).get()
+    return user_data
+
+def createUser(name):
+    users = getUsers()
+    id = len(users) + 1
+    user_data = {
+        "id": id,
+        "username": name,
+        "challenges": [],
+        "score": 0,
+        "message": ""
+    }
+    ref_users.child(str(id)).set(user_data)
+    return getUserById(id)
