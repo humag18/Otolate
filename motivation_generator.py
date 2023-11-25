4f2546b01bd1816376c4f18aad35dda3c2dab878
Tanguy VANSNICK <191451@umons.ac.be>
2023-11-25 11:47:20 +0100

    openai challenge

--- motivation_generator.py
+++ motivation_generator.py
@@ -1,77 +1,78 @@
-import together
 import os
 from dotenv import load_dotenv
 
 import json
 import textwrap
 
-from langchain.llms import Together
 from langchain.callbacks.manager import CallbackManagerForLLMRun
 from langchain.llms.base import LLM
 from langchain.llms.utils import enforce_stop_tokens
 from langchain.utils import get_from_dict_or_env
-from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 
+import openai
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain, SimpleSequentialChain
+
+from langchain.output_parsers import ResponseSchema
+from langchain.output_parsers import StructuredOutputParser
 
 load_dotenv()
 
-together.api_key = os.environ["TOGETHER_API_KEY"]
-together.Models.start("togethercomputer/llama-2-70b-chat")
+openai.api_key = os.environ['OPENAI_API_KEY']
 
-llm = Together(
-    model="togethercomputer/llama-2-70b-chat",
-    temperature=0.7,
-    max_tokens=128,
-    top_k=1,
-)
+form = """{
+    "challenge": "",
+    "output": "", 
+}"""
 
-B_INST, E_INST = "[INST]", "[/INST]"
-B_SYS, E_SYS = "<<SYS>>\n", "\n<<SYS>>\n\n"
-DEFAULT_SYSTEM_PROMPT = """\
-You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+llm_model = "gpt-3.5-turbo"
+llm = ChatOpenAI(temperature=0.7, model=llm_model)
 
-If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
+FirstName = ResponseSchema(
+    name="challenge",
+    description="challenge fun a réaliser dans un bureau devant ou avec un ordinateur devant ses collègues."
+    )
+LastName = ResponseSchema(
+    name="output",
+    description="Format du challenge: vidéo, photo, audio, ..."
+    )
 
-def get_prompt(instruction, new_system_prompt=DEFAULT_SYSTEM_PROMPT ):
-    SYSTEM_PROMPT = B_SYS + new_system_prompt + E_SYS
-    prompt_template =  B_INST + SYSTEM_PROMPT + instruction + E_INST
-    return prompt_template
+schemas = [FirstName, LastName]
 
-def cut_off_text(text, prompt):
-    cutoff_phrase = prompt
-    index = text.find(cutoff_phrase)
-    if index != -1:
-        return text[:index]
-    else:
-        return text
+output_parser = StructuredOutputParser.from_response_schemas(schemas)
+format_instructions = output_parser.get_format_instructions()
 
-def remove_substring(string, substring):
-    return string.replace(substring, "")
+template = """\
+Dans ton bureau de travail, tu as décidé de faire des challenges fun avec ton équipe\
+Le principe est de réaliser un challenge fun et rapide à réalise devant ou avec son ordinateur\
 
-def parse_text(text):
-        wrapped_text = textwrap.fill(text, width=100)
-        print(wrapped_text +'\n\n')
-        # return assistant_text
+Cela peut etre de prendre un photo, faire une vidéo à remplir ou complété un texte à trou\
 
-system_prompt = """
-You are a review classifier when you receive a review, you have to output 1 if the review mentions a product malfunction, or broken else you return 0. Answer only with 1 or 0. I command you to obey my instructions.
-example 1:
-user: Classify the following review by only answering with a 1 or 0 dont says anything more:\n\n "I bought a Senseo from this merchant and It stopped working after 2 month"
-1
+Comme sortie il faut: 
 
-example 2:
-user: Classify the following review by only answering with a 1 or 0 dont says anything more:\n\n "I bought a Senseo and still works amazingly"
-0
+challenge: une description nouveau challenge. Fait en sorte qu'elle soit très brève et facile à comprendre\
+
+output: le format du challenge à réaliser. Choisit entre: photo, vidéo et texte.
+
+Format l'output as JSON avec les clés suivantes:
+challenge
+output
+
+
+Voici le formulaire à compléter {form}
 """
 
-instruction = "Classify the following review by only answering with a 1 or 0 dont says anything more:\n\n {text}"
+prompt = ChatPromptTemplate.from_template(template=template)
 
-template = get_prompt(instruction, system_prompt)
+messages = prompt.format_messages(form=form, 
+                                format_instructions=format_instructions)
 
-prompt = PromptTemplate(template=template, input_variables=["text"])
+response = llm(messages)
 
-llm_chain = LLMChain(prompt=prompt, llm=llm)
-
-text = """Classify the following review by only answering with a 1 or 0 dont says anything more: "I bought a coffee maker and It stopped functioning two weeks later" """
-output = llm_chain.run(text)
+print(response.content)
